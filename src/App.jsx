06d0cb473b2e615813c4f64a900d97e447273c53
maite <maitecelcastelli@gmail.com>
2022-07-20 01:16:50 -0300

    Commit 20/07

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import NavBar from "./components/Navbar/NavBar"
+import clothesVdeo from './media/clothes_video.mp4'
+// import Carousel from './components/Carousel/Carousel'
+import clothes2 from './media/clothes2.jpg'
 
 function App() {
-
   return (
     <>
-    <NavBar/>
+      <NavBar />
+      <header>
+        <video autoPlay loop muted>
+          <source src={clothesVdeo} type='video/mp4' />
+        </video>
+        <div className="container h-100">
+          <div className="d-flex h-100 text-center align-items-center">
+            <div className="w-100 text-white">
+              <h1 className="display-3">THE NEW SEASON NOW IN</h1>
+              <a href="#">Shop now</a>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="NewProduct">
+    <img src={clothes2} alt="" />
+    <div className="NewProductDesc">
+    <h2>VESTIDO <br /> CASSANDRA  <br /> EDICIÓN  <br /> LIMITADA</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus quo magnam vitae debitis nemo, cumque repellendus? Laudantium minus voluptates sit nesciunt a nisi ad blanditiis hic provident quidem, fugiat veritatis.</p>
+  </div>
+  </div>
     </>
   );
 }
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
+import ReactLogo from '../../media/ReactLogo.jpg'
 
 const NavBar = () => {
   return (
-    <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">In Secret</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <div >
+      <div className= "d-flex justify-content-center mt-80">
+        <a href="#" className="Logo"><img src={ReactLogo} alt="RÉGIS" /></a>
+        </div>
+        <nav className="navbar navbar-expand-lg navbar-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
+  <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link" href="#">Home</a>
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
         <a class="nav-link" href="#">Get to know us</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Courses</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Courses</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact us</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact us</a>
       </li>
     </ul>
   </div>
