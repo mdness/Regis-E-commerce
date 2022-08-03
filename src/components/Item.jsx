24ef2948b7b0dd7cf58dcfc76d7d3c74fd6477b9
maite <maitecelcastelli@gmail.com>
2022-08-02 21:35:38 -0300

    Descriptions (2)

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ItemCount from './ItemCount'
+import Card from 'react-bootstrap/Card';
 
 const Item = ({name, imagen}) => {
   return (
-    <div className='divItem'>
-       {imagen}
-       {name}
-       <ItemCount/>
-    </div>
+    <Card className='itemCard'>
+      <Card.Img src={imagen} /> 
+      <Card.Body className='itemCardBody'>
+        <Card.Title className='itemCardBodyTitle'>{name}</Card.Title>    
+       <div className='itemCardBodyDiv'>
+        <ItemCount/>
+       </div>
+    </Card.Body>
+    </Card>
   )
 }
 
